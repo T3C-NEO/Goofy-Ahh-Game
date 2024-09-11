f5022051d60ef4cabaf9954e6c0da29038c3f80a
neo <neoshon421@gmail.com>
2024-09-11 18:15:57 -0400

    wobble

---
+++ Assets/wobble.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wobble : MonoBehaviour
+{
+    public Rigidbody rb;
+    Vector3 turnSpeed = new Vector3(0,0,0.4f);
+    Vector3 turnSpeedX = new Vector3(0.4f,0,0);
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (transform.rotation.eulerAngles.z > 5 && transform.rotation.eulerAngles.z < 150)
+        {
+            rb.angularVelocity -= turnSpeed;
+        } else if (transform.rotation.eulerAngles.z < 355 && transform.rotation.eulerAngles.z > 200)
+        {
+            rb.angularVelocity += turnSpeed;
+        }
+        
+        if (transform.rotation.eulerAngles.x > 5 && transform.rotation.eulerAngles.x < 150)
+        {
+            rb.angularVelocity -= turnSpeedX;
+        } else if (transform.rotation.eulerAngles.x < 355 && transform.rotation.eulerAngles.x > 200)
+        {
+            rb.angularVelocity += turnSpeedX;
+        }
+        Debug.Log(rb.angularVelocity);
+    }
+}
