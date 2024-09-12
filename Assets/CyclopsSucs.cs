64d03c40814fb09916f073d838d9cce1c7d12940
T3CNEO <89314532+T3C-NEO@users.noreply.github.com>
2024-09-12 16:39:41 -0400

    class work

--- Assets/CyclopsSucs.cs
+++ Assets/CyclopsSucs.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class CyclopsSucs : MonoBehaviour
 {
@@ -11,17 +13,44 @@ public class CyclopsSucs : MonoBehaviour
     public GameObject prefab;
 
     int score = 0;
-    public GameObject tex;
+    public TMP_Text scoreText;
 
-    public GameObject rota;
+    public Rigidbody wobbler;
 
-    void Start()
-    {
-    }
+    public int tool;
 
     // Update is called once per frame
-    void Update()
+    public void Tool0()
     {
+        tool = 0;
+    }
+    public void Tool1()
+    {
+        tool = 1;
+    }
+    public void Tool2()
+    {
+        tool = 2;
+    }
+    public void Tool3()
+    {
+        tool = 3;
+    }
+    public void Tool4()
+    {
+        tool = 4;
+    }
+    public void Tool5()
+    {
+        tool = 5;
+    }
+    void FixedUpdate()
+    {
+        if (Math.Max(Math.Max(wobbler.velocity.x, wobbler.velocity.y), wobbler.velocity.z) > 2)
+        {
+            score += (int)Math.Max(Math.Max(wobbler.velocity.x, wobbler.velocity.y), wobbler.velocity.z);
+        }
+        scoreText.text = score.ToString();
         if (Input.GetKey("d"))
         {
             transform.Rotate(new Vector3(0, -2, 0));
@@ -37,39 +66,32 @@ public class CyclopsSucs : MonoBehaviour
         {
             if (Input.GetMouseButton(0))
             {
-                if (hit.rigidbody != null && hit.rigidbody.gameObject.tag == "Targ")
+                if (hit.rigidbody != null)
                 {
-                    Destroy(hit.rigidbody.gameObject);
-                    score += 1;
-
-                }
-                else if (hit.rigidbody != null && hit.rigidbody.gameObject.tag == "NO")
-                {
-                    SceneManager.LoadScene("SampleScene");
-                }
-                else if (hit.rigidbody != null)
-                {
-
-                    hit.rigidbody.AddExplosionForce(100, hit.point, 5f);
+                    if (tool == 0)
+                    {
+                        hit.rigidbody.AddExplosionForce(500, hit.point, 50f);
+                    } else if (tool == 1)
+                    {
+                        hit.rigidbody.AddExplosionForce(500, wobbler.transform.position, 5f);
+                    }
+                    else if (tool == 2 && hit.rigidbody.tag == "Finish")
+                    {
+                        Instantiate(prefab, hit.point, Quaternion.identity);
+                    }
+                    else if (tool == 3)
+                    {
+                        hit.rigidbody.AddExplosionForce(-500, hit.point, 5f);
+                    }
+                    //make one that teleports it randomly in the box and deletes the rats under it
+                    //add that it does that one if it gets forced out of the box
                 }
             }
-
+            /*
             if (Input.GetMouseButtonDown(1))
             {
                 Instantiate(prefab, hit.point, Quaternion.identity);
-            }
-        }
-        if (score == 7)
-        {
-            tex.SetActive(true);
-        }
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        //Check to see if the tag on the collider is equal to Enemy
-        if (other.tag == "Enemy")
-        {
-            SceneManager.LoadScene("SampleScene");
+            }*/
         }
     }
 }
